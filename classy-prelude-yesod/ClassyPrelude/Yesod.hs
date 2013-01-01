9f7e45a42efd8f22d975fbc41eff089895dd824f
Michael Snoyman <michael@snoyman.com>
2013-01-01 15:16:14 +0200

    insertDB and voidKey

--- classy-prelude-yesod/ClassyPrelude/Yesod.hs
+++ classy-prelude-yesod/ClassyPrelude/Yesod.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE TypeFamilies #-}
 module ClassyPrelude.Yesod
     ( module X
+    , module ClassyPrelude.Yesod
     ) where
 
 import ClassyPrelude.Conduit as X hiding (lift)
@@ -23,6 +24,32 @@ instance
   , entity ~ entity'
   ) => ClassyPrelude.Classes.CanInsert (entity -> m (KeyBackend backend entity')) where
     insert = Yesod.insert
+
+-- | Non-typeclass insert for use with Persistent.
+--
+-- If you don\'t use the @Key@ returned from @insert@, the type inferencing
+-- cannot tell which @insert@ function to use. Using @insertDB@ disambiguates.
+-- Another options is using 'voidKey'.
+--
+-- Since 0.1.0.0
+insertDB :: ( PersistStore m
+            , PersistEntity entity
+            , PersistMonadBackend m ~ PersistEntityBackend entity
+            )
+         => entity
+         -> m (Key entity)
+insertDB = Yesod.insert
+
+-- | Ignore the @Key@ returned by insert.
+--
+-- See 'insertDB' for more information. The following two lines are equivalent:
+--
+-- > _ <- insertDB foo
+-- > voidKey $ insert foo
+--
+-- Since 0.1.0.0
+voidKey :: Functor m => m (KeyBackend backend entity) -> m ()
+voidKey = void
 
 instance
   ( backend ~ PersistMonadBackend m
