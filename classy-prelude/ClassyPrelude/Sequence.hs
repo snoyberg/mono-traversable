0a06c5012129ad617e42f6ebddd2b15c62eeb92c
Nikita Volkov <nikita.y.volkov@mail.ru>
2013-03-02 21:03:36 +0400

    Fix qualified alias for easier refactoring

--- classy-prelude/ClassyPrelude/Sequence.hs
+++ classy-prelude/ClassyPrelude/Sequence.hs
@@ -12,7 +12,7 @@ import qualified Data.Monoid as Monoid
 import qualified Control.Monad as Monad
 import qualified Data.Foldable as Foldable
 import qualified Data.Traversable as Traversable
-import qualified Data.Sequence as Sequence
+import qualified Data.Sequence as Seq
 import Data.Sequence (Seq, (<|), (|>), ViewL(..), viewl, ViewR(..), viewr)
 
 
@@ -23,34 +23,34 @@ instance CanConcatMap (Seq a) (Seq b) a (Seq b) where
     concatMap = (Monad.=<<)
 
 instance CanFilter (Seq a) a where
-    filter = Sequence.filter
+    filter = Seq.filter
 
 instance CanFilterM (Seq a) a where
     filterM p =
         Foldable.foldlM
             (\ xs x -> do res <- p x; return $ if res then xs |> x else xs)
-            Sequence.empty
+            Seq.empty
 
 instance CanLength (Seq a) Int where
-    length = Sequence.length
+    length = Seq.length
 
 instance CanSingleton (Seq a) a where
-    singleton = Sequence.singleton
+    singleton = Seq.singleton
 
 instance CanNull (Seq a) where
-    null = Sequence.null
+    null = Seq.null
 
 instance CanPack (Seq a) a where
-    pack = Sequence.fromList
+    pack = Seq.fromList
     unpack = Foldable.toList
 
 instance CanIntersperse (Seq a) a where
     intersperse sep xs = case viewl xs of
-        EmptyL -> Sequence.empty
+        EmptyL -> Seq.empty
         (h :< t) -> h <| prependToAll sep t
       where
         prependToAll sep xs = case viewl xs of
-            EmptyL -> Sequence.empty
+            EmptyL -> Seq.empty
             (h :< t) -> sep <| (h <| prependToAll sep t)
 
 instance Eq a => CanStripPrefix (Seq a) where
@@ -58,20 +58,20 @@ instance Eq a => CanStripPrefix (Seq a) where
         | x == y1 = Just y2
         | otherwise = Nothing
       where
-        (y1, y2) = Sequence.splitAt (Sequence.length x) y
-    isPrefixOf x y = Sequence.take (Sequence.length x) y == x
+        (y1, y2) = Seq.splitAt (Seq.length x) y
+    isPrefixOf x y = Seq.take (Seq.length x) y == x
 
 instance Eq a => CanStripSuffix (Seq a) where
     stripSuffix x y
         | x == y2 = Just y1
         | otherwise = Nothing
       where
-        (y1, y2) = Sequence.splitAt (Sequence.length y - Sequence.length x) y
-    isSuffixOf x y = takeRR (Sequence.length x) y == x
+        (y1, y2) = Seq.splitAt (Seq.length y - Seq.length x) y
+    isSuffixOf x y = takeRR (Seq.length x) y == x
       where
-        takeRR 0 _ = Sequence.empty
+        takeRR 0 _ = Seq.empty
         takeRR n xs = case viewr xs of
-            EmptyR -> Sequence.empty
+            EmptyR -> Seq.empty
             xs' :> x -> takeRR (n-1) xs' |> x
 
 instance Monad m => CanMapM (Seq i) (m (Seq o)) m i o where
@@ -84,33 +84,33 @@ instance Eq x => CanMember (Seq x) x where
     member x = any (== x)
 
 instance CanBreak (Seq a) a where
-    break = Sequence.breakl
-    span = Sequence.spanl
-    dropWhile = Sequence.dropWhileL
-    takeWhile = Sequence.takeWhileL
+    break = Seq.breakl
+    span = Seq.spanl
+    dropWhile = Seq.dropWhileL
+    takeWhile = Seq.takeWhileL
 
 instance CanAny (Seq a) a where
     any = Foldable.any
     all = Foldable.all
 
 instance CanSplitAt (Seq a) Int where
-    splitAt = Sequence.splitAt
+    splitAt = Seq.splitAt
 
 instance CanFold (Seq a) a accum where
     fold = Foldable.foldl'
 
 instance CanReverse (Seq a) where
-    reverse = Sequence.reverse
+    reverse = Seq.reverse
 
 instance CanReplicate (Seq a) a Int where
-    replicate = Sequence.replicate
+    replicate = Seq.replicate
 
 instance CanReplicateM (Seq a) a Int where
-    replicateM = Sequence.replicateM
+    replicateM = Seq.replicateM
 
 instance CanFind (Seq a) a where
-    find p s = case Sequence.breakl p s of
-        (_, s') -> case Sequence.viewl s' of
+    find p s = case Seq.breakl p s of
+        (_, s') -> case Seq.viewl s' of
             a :< _ -> Just a
             _ -> Nothing
 
@@ -118,18 +118,18 @@ instance (Monoid m) => CanConcat (Seq m) m where
     concat = Foldable.fold
 
 instance CanPartition (Seq a) a where
-    partition = Sequence.partition
+    partition = Seq.partition
 
 instance CanSortBy (Seq a) a where
-    sortBy = Sequence.sortBy
+    sortBy = Seq.sortBy
 
 instance Ord a => CanSort (Seq a) a where
-    sort = Sequence.sort
+    sort = Seq.sort
 
 instance CanCons (Seq a) a where
     cons = (<|)
 
 instance CanUncons (Seq a) a where
-    uncons s = case Sequence.viewl s of
+    uncons s = case Seq.viewl s of
         EmptyL -> Nothing
         a :< s' -> Just (a, s')
