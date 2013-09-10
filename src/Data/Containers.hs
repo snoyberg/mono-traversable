d00566bf7673251d806e0bfa0ac5fe8fabe56c2f
Michael Snoyman <michael@fpcomplete.com>
2013-09-10 07:50:14 +0000

    Type families for Data.Containers

--- src/Data/Containers.hs
+++ src/Data/Containers.hs
@@ -1,6 +1,6 @@
-{-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE FunctionalDependencies #-}
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE TypeFamilies #-}
 module Data.Containers where
 
 import qualified Data.Map as Map
@@ -9,64 +9,69 @@ import Data.Hashable (Hashable)
 import qualified Data.Set as Set
 import qualified Data.HashSet as HashSet
 import Data.Monoid (Monoid)
-import Data.MonoTraversable (MonoFoldable, MonoTraversable)
+import Data.MonoTraversable (MonoFoldable, MonoTraversable, Element)
 
-class (Monoid c, MonoFoldable c) => Container c k | c -> k where
-    member :: k -> c -> Bool
-    notMember :: k -> c -> Bool
+class (Monoid c, MonoFoldable c) => Container c where
+    type ContainerKey c
+    member :: ContainerKey c -> c -> Bool
+    notMember ::  ContainerKey c -> c -> Bool
     union :: c -> c -> c
     difference :: c -> c -> c
     intersection :: c -> c -> c
-instance Ord k => Container (Map.Map k v) k where
+instance Ord k => Container (Map.Map k v) where
+    type ContainerKey (Map.Map k v) = k
     member = Map.member
     notMember = Map.notMember
     union = Map.union
     difference = Map.difference
     intersection = Map.intersection
-instance (Eq k, Hashable k) => Container (HashMap.HashMap k v) k where
+instance (Eq k, Hashable k) => Container (HashMap.HashMap k v) where
+    type ContainerKey (HashMap.HashMap k v) = k
     member = HashMap.member
     notMember k = not . HashMap.member k
     union = HashMap.union
     difference = HashMap.difference
     intersection = HashMap.intersection
-instance Ord e => Container (Set.Set e) e where
+instance Ord e => Container (Set.Set e) where
+    type ContainerKey (Set.Set e) = e
     member = Set.member
     notMember = Set.notMember
     union = Set.union
     difference = Set.difference
     intersection = Set.intersection
-instance (Eq e, Hashable e) => Container (HashSet.HashSet e) e where
+instance (Eq e, Hashable e) => Container (HashSet.HashSet e) where
+    type ContainerKey (HashSet.HashSet e) = e
     member = HashSet.member
     notMember e = not . HashSet.member e
     union = HashSet.union
     difference = HashSet.difference
     intersection = HashSet.intersection
 
-class (MonoTraversable m, Container m k) => IsMap m k v | m -> k v where
-    lookup :: k -> m -> Maybe v
-    insertMap :: k -> v -> m -> m
-    deleteMap :: k -> m -> m
-    singletonMap :: k -> v -> m
-instance Ord k => IsMap (Map.Map k v) k v where
+class (MonoTraversable m, Container m) => IsMap m where
+    lookup :: ContainerKey m -> m -> Maybe (Element m)
+    insertMap :: ContainerKey m -> Element m -> m -> m
+    deleteMap :: ContainerKey m -> m -> m
+    singletonMap :: ContainerKey m -> Element m -> m
+instance Ord k => IsMap (Map.Map k v) where
     lookup = Map.lookup
     insertMap = Map.insert
     deleteMap = Map.delete
     singletonMap = Map.singleton
-instance (Eq k, Hashable k) => IsMap (HashMap.HashMap k v) k v where
+instance (Eq k, Hashable k) => IsMap (HashMap.HashMap k v) where
     lookup = HashMap.lookup
     insertMap = HashMap.insert
     deleteMap = HashMap.delete
     singletonMap = HashMap.singleton
 
-class Container s e => IsSet s e | s -> e where
-    insertSet :: e -> s -> s
-    deleteSet :: e -> s -> s
-    singletonSet :: e -> s
-instance Ord e => IsSet (Set.Set e) e where
+class (Container s, Element s ~ ContainerKey s) => IsSet s where
+    insertSet :: Element s -> s -> s
+    deleteSet :: Element s -> s -> s
+    singletonSet :: Element s -> s
+instance Ord e => IsSet (Set.Set e) where
     insertSet = Set.insert
     deleteSet = Set.delete
     singletonSet = Set.singleton
-instance (Eq e, Hashable e) => IsSet (HashSet.HashSet e) e where
+instance (Eq e, Hashable e) => IsSet (HashSet.HashSet e) where
     insertSet = HashSet.insert
     deleteSet = HashSet.delete
     singletonSet = HashSet.singleton
