29bb4d070295f84a5aba9d87d7dea7255cb0c5df
Michael Snoyman <michael@snoyman.com>
2013-09-12 10:48:44 +0300

    Get everything compiling again

--- classy-prelude-conduit/ClassyPrelude/Conduit.hs
+++ classy-prelude-conduit/ClassyPrelude/Conduit.hs
@@ -7,25 +7,9 @@ module ClassyPrelude.Conduit
       module ClassyPrelude
     , module Data.Conduit
     , module Data.Conduit.List
-      -- * XML
-    , X.Document (..)
-    , X.Name (..)
-    , X.Prologue (..)
-    , X.Node (..)
-    , X.Element (..)
     ) where
 
 import ClassyPrelude
-import ClassyPrelude.Classes
 
 import Data.Conduit
 import Data.Conduit.List (consume, sinkNull)
-import qualified Data.Conduit.Binary as CB
-
-import qualified Text.XML as X
-
-instance CanReadFile X.Document where
-    readFile = liftIO . X.readFile X.def
-
-instance CanWriteFile X.Document where
-    writeFile fp = liftIO . X.writeFile X.def fp
--- classy-prelude-yesod/ClassyPrelude/Yesod.hs
+++ classy-prelude-yesod/ClassyPrelude/Yesod.hs
@@ -3,11 +3,10 @@
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 module ClassyPrelude.Yesod
     ( module X
-    , module ClassyPrelude.Yesod
     ) where
 
 import ClassyPrelude.Conduit as X hiding (lift)
-import Yesod as X hiding (Header, insert, delete)
+import Yesod as X hiding (Header)
 import qualified Yesod
 import Yesod.Static as X
 import Yesod.Feed as X
@@ -17,47 +16,3 @@ import qualified ClassyPrelude.Classes
 import Database.Persist.Sql as X (SqlBackend, SqlPersistT)
 import Database.Persist.Sql as X (runMigration)
 import Data.Default as X (Default (..))
-
-instance
-  ( backend ~ PersistMonadBackend m
-  , backend ~ PersistEntityBackend entity
-  , PersistStore m
-  , PersistEntity entity
-  , entity ~ entity'
-  ) => ClassyPrelude.Classes.CanInsert (entity -> m (KeyBackend backend entity')) where
-    insert = Yesod.insert
-
--- | Non-typeclass insert for use with Persistent.
---
--- If you don\'t use the @Key@ returned from @insert@, the type inferencing
--- cannot tell which @insert@ function to use. Using @insertDB@ disambiguates.
--- Another options is using 'voidKey'.
---
--- Since 0.1.0.0
-insertDB :: ( PersistStore m
-            , PersistEntity entity
-            , PersistMonadBackend m ~ PersistEntityBackend entity
-            )
-         => entity
-         -> m (Key entity)
-insertDB = Yesod.insert
-
--- | Ignore the @Key@ returned by insert.
---
--- See 'insertDB' for more information. The following two lines are equivalent:
---
--- > _ <- insertDB foo
--- > voidKey $ insert foo
---
--- Since 0.1.0.0
-voidKey :: Functor m => m (KeyBackend backend entity) -> m ()
-voidKey = void
-
-instance
-  ( backend ~ PersistMonadBackend m
-  , backend ~ PersistEntityBackend entity
-  , PersistStore m
-  , PersistEntity entity
-  , a ~ ()
-  ) => ClassyPrelude.Classes.CanDelete (KeyBackend backend entity -> m a) where
-    delete = Yesod.delete
