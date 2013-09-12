a5f60fe57aa4fe34c6e9f3cde4b45feb8ed60f56
Michael Snoyman <michael@snoyman.com>
2013-09-12 08:49:45 +0300

    Fix tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -12,28 +12,25 @@ import Prelude (Bool (..), ($), IO, min, abs, Eq (..), (&&), fromIntegral, Ord (
 main :: IO ()
 main = hspec $ do
     describe "cnull" $ do
-        it "empty list" $ cnull [] `shouldBe` True
-        it "non-empty list" $ cnull [()] `shouldBe` False
-        it "empty text" $ cnull ("" :: Text) `shouldBe` True
-        it "non-empty text" $ cnull ("foo" :: Text) `shouldBe` False
+        it "empty list" $ onull [] `shouldBe` True
+        it "non-empty list" $ onull [()] `shouldBe` False
+        it "empty text" $ onull ("" :: Text) `shouldBe` True
+        it "non-empty text" $ onull ("foo" :: Text) `shouldBe` False
     describe "clength" $ do
         prop "list" $ \i' ->
             let x = replicate i () :: [()]
                 i = min 500 $ abs i'
-             in clength x == i &&
-                clength64 x == fromIntegral i
+             in olength x == i
         prop "text" $ \i' ->
             let x = replicate i 'a' :: Text
                 i = min 500 $ abs i'
-             in clength x == i &&
-                clength64 x == fromIntegral i
+             in olength x == i
         prop "lazy bytestring" $ \i' ->
             let x = replicate i 6 :: L.ByteString
                 i = min 500 $ abs i'
-             in clength x == i &&
-                clength64 x == fromIntegral i
+             in olength64 x == i
     describe "ccompareLength" $ do
         prop "list" $ \i' j ->
             let i = min 500 $ abs i'
                 x = replicate i () :: [()]
-             in ccompareLength x j == compare i j
+             in ocompareLength x j == compare i j
