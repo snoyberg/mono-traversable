22f447f8175eddfdf2087b22ef3de00b78765382
Michael Snoyman <michael@snoyman.com>
2014-01-30 10:17:53 +0200

    gen-unqualified in hpc.sh

--- hpc.sh
+++ hpc.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -ex
 
 rm -f test.tix test.mix
+runghc gen-unqualified.hs > Data/Conduit/Combinators/Unqualified.hs
 cabal configure --ghc-option=-fhpc --enable-tests
 cabal build
 ./dist/build/test/test
