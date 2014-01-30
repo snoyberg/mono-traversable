c71df20c96a7ed9361a62a64b3fcbab21cf78d95
Michael Snoyman <michael@snoyman.com>
2014-01-30 13:27:37 +0200

    Remove Sequence constraint on NonNull.
    This was done to allow NonNull to be used for such things as Set and
    Map.

--- src/Data/NonNull.hs
+++ src/Data/NonNull.hs
@@ -18,6 +18,13 @@
 -- Please send your feedback.
 module Data.NonNull (
     NonNull(..)
+  , fromNonEmpty
+  , ncons
+  , nuncons
+  , splitFirst
+  , nfilter
+  , nfilterM
+  , nReplicate
   , head
   , tail
   , last
@@ -47,8 +54,9 @@ import qualified Data.List.NonEmpty as NE
 data NullError = NullError String deriving (Show, Typeable)
 instance Exception NullError
 
--- | a 'NonNull' sequence has 1 or more items
--- In contrast, 'IsSequence' is allowed to have zero items.
+-- | a 'NonNull' has 1 or more items
+--
+-- In contrast, 'MonoFoldable' is allowed to have zero items.
 --
 -- Any NonNull functions that
 -- decreases the number of elements in the sequences
@@ -62,19 +70,16 @@ instance Exception NullError
 -- With NonNull rather than always reacting with null checks we can proactively encode in our program when we know that a type is NonNull.
 -- Now we have an invariant encoded in our types, making our program easier to understand.
 -- This information is leveraged to avoid awkward null checking later on.
-class (SemiSequence seq, IsSequence (Nullable seq), Element seq ~ Element (Nullable seq)) => NonNull seq where
-    type Nullable seq
-
-    -- | safely construct a 'NonNull' sequence from a 'NonEmpty' list
-    fromNonEmpty :: NE.NonEmpty (Element seq) -> seq
+class (MonoFoldable mono, MonoFoldable (Nullable mono), Element mono ~ Element (Nullable mono)) => NonNull mono where
+    type Nullable mono
 
     -- | safely convert a 'Nullable' to a 'NonNull'
-    fromNullable :: Nullable seq -> Maybe seq
+    fromNullable :: Nullable mono -> Maybe mono
 
     -- | convert a 'Nullable' with elements to a 'NonNull'
     -- throw an exception if the 'Nullable' is empty.
     -- do not use this unless you have proved your structure is non-null
-    nonNull :: Nullable seq -> seq
+    nonNull :: Nullable mono -> mono
     nonNull nullable = case fromNullable nullable of
                          Nothing -> throw $ NullError "Data.NonNull.nonNull (NonNull default): expected non-null"
                          Just xs -> xs
@@ -85,41 +90,55 @@ class (SemiSequence seq, IsSequence (Nullable seq), Element seq ~ Element (Nulla
     -- nonNullUnsafe :: Nullable seq -> seq
 
     -- | convert a 'NonNull' to a 'Nullable'
-    toNullable :: seq -> Nullable seq
+    toNullable :: mono -> Nullable mono
 
-    -- | Like cons, prepends an element.
-    -- However, the prepend is to a Nullable, creating a 'NonNull'
-    --
-    -- Generally this uses cons underneath.
-    -- cons is not efficient for most data structures.
-    --
-    -- Alternatives:
-    --   * if you don't need to cons, use 'fromNullable' or 'nonNull' if you can create your structure in one go.
-    --   * if you need to cons, you might be able to start off with an efficient data structure such as a 'NonEmpty' List.
-    --     'fronNonEmpty' will convert that to your data structure using the structure's fromList function.
-    ncons :: Element seq -> Nullable seq -> seq
+-- | safely construct a 'NonNull' from a 'NonEmpty' list
+fromNonEmpty :: (NonNull seq, IsSequence (Nullable seq)) => NE.NonEmpty (Element seq) -> seq
+fromNonEmpty = nonNull . fromList . NE.toList
+{-# INLINE fromNonEmpty #-}
 
-    -- | like 'uncons' of 'SemiSequence'
-    nuncons :: seq -> (Element seq, Maybe seq)
-    nuncons xs = case uncons $ toNullable xs of
-                   Nothing -> error "Data.NonNull.nuncons: data structure is null, it should be non-null"
-                   Just (x, xsNullable) -> (x, fromNullable xsNullable)
+-- | Like cons, prepends an element.
+-- However, the prepend is to a Nullable, creating a 'NonNull'
+--
+-- Generally this uses cons underneath.
+-- cons is not efficient for most data structures.
+--
+-- Alternatives:
+--   * if you don't need to cons, use 'fromNullable' or 'nonNull' if you can create your structure in one go.
+--   * if you need to cons, you might be able to start off with an efficient data structure such as a 'NonEmpty' List.
+--     'fronNonEmpty' will convert that to your data structure using the structure's fromList function.
+ncons :: (NonNull seq, SemiSequence (Nullable seq)) => Element seq -> Nullable seq -> seq
+ncons x xs = nonNull $ cons x xs
 
-    -- | like 'uncons' of 'SemiSequence'
-    splitFirst :: seq -> (Element seq, Nullable seq)
-    splitFirst xs = case uncons $ toNullable xs of
-                     Nothing -> error "Data.NonNull.splitFirst: data structure is null, it should be non-null"
-                     Just tup -> tup
+-- | like 'uncons' of 'SemiSequence'
+nuncons :: (NonNull seq, IsSequence (Nullable seq)) => seq -> (Element seq, Maybe seq)
+nuncons xs = case uncons $ toNullable xs of
+               Nothing -> error "Data.NonNull.nuncons: data structure is null, it should be non-null"
+               Just (x, xsNullable) -> (x, fromNullable xsNullable)
+
+-- | like 'uncons' of 'SemiSequence'
+splitFirst :: (IsSequence (Nullable seq), NonNull seq) => seq -> (Element seq, Nullable seq)
+splitFirst xs = case uncons $ toNullable xs of
+                 Nothing -> error "Data.NonNull.splitFirst: data structure is null, it should be non-null"
+                 Just tup -> tup
 
 
-    -- | like 'Sequence.filter', but starts with a NonNull
-    nfilter :: (Element seq -> Bool) -> seq -> Nullable seq
+-- | like 'Sequence.filter', but starts with a NonNull
+nfilter :: (NonNull seq, IsSequence (Nullable seq))
+        => (Element seq -> Bool) -> seq -> Nullable seq
+nfilter f = filter f . toNullable
 
-    -- | like 'Sequence.filterM', but starts with a NonNull
-    nfilterM :: Monad m => (Element seq -> m Bool) -> seq -> m (Nullable seq)
+-- | like 'Sequence.filterM', but starts with a NonNull
+nfilterM :: (NonNull seq, Monad m, IsSequence (Nullable seq))
+         => (Element seq -> m Bool) -> seq -> m (Nullable seq)
+nfilterM f = filterM f . toNullable
 
-    -- | i must be > 0. like 'Sequence.replicate'
-    nReplicate :: Index seq -> Element seq -> seq
+-- | i must be > 0. like 'Sequence.replicate'
+--
+-- i <= 0 is treated the same as providing 1
+nReplicate :: (NonNull seq, Num (Index (Nullable seq)), Ord (Index (Nullable seq)), IsSequence (Nullable seq))
+           => Index (Nullable seq) -> Element seq -> seq
+nReplicate i = nonNull . replicate (max 1 i)
 
 {-
 maybeToNullable :: (Monoid (Nullable seq), NonNull seq) => Maybe seq -> Nullable seq
@@ -128,22 +147,22 @@ maybeToNullable (Just xs) = toNullable xs
 -}
 
 -- | like Data.List, but not partial on a NonEmpty
-head :: NonNull seq => seq -> Element seq
+head :: (IsSequence (Nullable seq), NonNull seq) => seq -> Element seq
 head = partialHead . toNullable
 {-# INLINE head #-}
 
 -- | like Data.List, but not partial on a NonEmpty
-tail :: NonNull seq => seq -> Nullable seq
+tail :: (IsSequence (Nullable seq), NonNull seq) => seq -> Nullable seq
 tail = partialTail . toNullable
 {-# INLINE tail #-}
 
 -- | like Data.List, but not partial on a NonEmpty
-last :: NonNull seq => seq -> Element seq
+last :: (IsSequence (Nullable seq), NonNull seq) => seq -> Element seq
 last = partialLast . toNullable
 {-# INLINE last #-}
 
 -- | like Data.List, but not partial on a NonEmpty
-init :: NonNull seq => seq -> Nullable seq
+init :: (IsSequence (Nullable seq), NonNull seq) => seq -> Nullable seq
 init = partialInit . toNullable
 {-# INLINE init #-}
 
@@ -153,24 +172,12 @@ init = partialInit . toNullable
 instance NonNull (NE.NonEmpty a) where
     type Nullable (NE.NonEmpty a) = [a]
 
-    fromNonEmpty = id
-    {-# INLINE fromNonEmpty #-}
     fromNullable = NE.nonEmpty
 
     nonNull = NE.fromList
     -- nonNullUnsafe = nonNull
 
     toNullable = NE.toList
-
-    ncons = (NE.:|)
-
-    nfilter = NE.filter
-    nfilterM f = filterM f . toNullable
-
-    nReplicate i x = NE.unfold unfold i
-      where
-        unfold countdown | countdown < 1 = (x, Nothing)
-                         | otherwise     = (x, Just (countdown - 1))
 
 
 -- | a newtype wrapper indicating there are 1 or more elements
@@ -209,10 +216,9 @@ instance SemiSequence seq => SemiSequence (NotEmpty seq) where
 
 -- normally we favor defaulting, should we use it here?
 -- this re-uses IsSequence functions and IsSequence uses defaulting
-instance IsSequence seq => NonNull (NotEmpty seq) where
+instance MonoFoldable seq => NonNull (NotEmpty seq) where
     type Nullable (NotEmpty seq) = seq
 
-    fromNonEmpty = NotEmpty . fromList . NE.toList
     fromNullable xs | onull xs = Nothing
                     | otherwise = Just $ NotEmpty xs
 
@@ -221,21 +227,12 @@ instance IsSequence seq => NonNull (NotEmpty seq) where
 
     -- nonNullUnsafe = NotEmpty
     toNullable = fromNotEmpty
-    ncons x xs = NotEmpty $ cons x xs
-
-    -- | i must be > 0. like 'Sequence.replicate'
-    -- < 0 produces a 1 element NonEmpty
-    nReplicate i x | i < 1 = ncons x mempty
-                   | otherwise = NotEmpty $ replicate i x
-
-    nfilter f = filter f . toNullable
-    nfilterM f = filterM f . toNullable
 
 infixr 5 <|
 
 -- | Prepend an element to a NonNull
-(<|) :: NonNull seq => Element seq -> seq -> seq
-(<|) = cons
+(<|) :: (SemiSequence (Nullable seq), NonNull seq) => Element seq -> seq -> seq
+x <| y = ncons x (toNullable y)
 
 
 ofoldMap1 :: (NonNull seq, Semigroup m) => (Element seq -> m) -> seq -> m
