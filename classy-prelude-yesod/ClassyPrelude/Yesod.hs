1e2e50a7ee60d155881947b75d20979b1b632b76
Michael Snoyman <michael@snoyman.com>
2014-04-09 21:50:06 +0300

    Fix for GHC 7.4

--- classy-prelude-yesod/ClassyPrelude/Yesod.hs
+++ classy-prelude-yesod/ClassyPrelude/Yesod.hs
@@ -5,7 +5,7 @@ module ClassyPrelude.Yesod
     ( module X
     ) where
 
-import ClassyPrelude.Conduit as X hiding (lift, check)
+import ClassyPrelude.Conduit as X
 import Yesod as X hiding (Header, parseTime)
 import qualified Yesod
 import Yesod.Static as X
