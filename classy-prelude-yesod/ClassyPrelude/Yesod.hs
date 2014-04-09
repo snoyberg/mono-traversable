9b83d0df988966696d9568f3781df2fd73ab2e1d
Michael Snoyman <michael@snoyman.com>
2014-04-09 18:35:00 +0300

    Export Network.HTTP.Client.Conduit instead
    @gregwebs Any objection to this?

--- classy-prelude-yesod/ClassyPrelude/Yesod.hs
+++ classy-prelude-yesod/ClassyPrelude/Yesod.hs
@@ -10,7 +10,7 @@ import Yesod as X hiding (Header, parseTime)
 import qualified Yesod
 import Yesod.Static as X
 import Yesod.Feed as X
-import Network.HTTP.Conduit as X
+import Network.HTTP.Client.Conduit as X
 import Network.HTTP.Types as X
 import Database.Persist.Sql as X (SqlBackend, SqlPersistT)
 import Database.Persist.Sql as X (runMigration)
