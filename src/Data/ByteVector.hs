3b0d6145e23853f4ab484f8adc117fadf897fb8b
Kyle Billings <kyleabillings@gmail.com>
2015-02-21 22:21:36 -0500

    Fix Data.ByteVector documentation linking

--- src/Data/ByteVector.hs
+++ src/Data/ByteVector.hs
@@ -1,5 +1,5 @@
--- | Provides conversion functions between strict @ByteString@s and storable
--- @Vector@s.
+-- | Provides conversion functions between strict 'ByteString's and storable
+-- 'Vector's.
 module Data.ByteVector
     ( toByteVector
     , fromByteVector
@@ -10,14 +10,14 @@ import           Data.Vector.Storable     (Vector, unsafeFromForeignPtr,
                                            unsafeToForeignPtr)
 import           Data.Word                (Word8)
 
--- | Convert a @ByteString@ into a storable @Vector@.
+-- | Convert a 'ByteString' into a storable 'Vector'.
 --
 -- Since 0.6.1
 toByteVector :: ByteString -> Vector Word8
 toByteVector (PS fptr offset idx) = unsafeFromForeignPtr fptr offset idx
 {-# INLINE toByteVector #-}
 
--- | Convert a storable @Vector@ into a @ByteString@.
+-- | Convert a storable 'Vector' into a 'ByteString'.
 --
 -- Since 0.6.1
 fromByteVector :: Vector Word8 -> ByteString
