384d87f28d7c91e68a97ed5db8526c5d38fbcb51
Benjamin <benjamin.mcrae@tracsis.com>
2024-11-20 12:59:04 +0000

    No need to have a full set container, we can use semisetcontainer as the super class for haskeysset

--- mono-traversable/src/Data/Containers.hs
+++ mono-traversable/src/Data/Containers.hs
@@ -820,7 +820,7 @@ instance MonoZip LText.Text where
     {-# INLINE ozipWith #-}
 
 -- | Type class for maps whose keys can be converted into sets.
-class SetContainer set => HasKeysSet set where
+class SemiSetContainer set => HasKeysSet set where
     -- | Type of the key set.
     type KeySet set
 
